c8767c406f2ef3d447b4b28eac31302626a7edac
Sam Westley <swwestley@gmail.com>
2013-08-04 20:47:08 +0000

    copied node back-end to experiments

---
+++ experiments/test_server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var mybuf = fs.readFileSync("test.html");
+  response.send(mybuf.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
